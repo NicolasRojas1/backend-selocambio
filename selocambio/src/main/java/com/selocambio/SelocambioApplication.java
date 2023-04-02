478b38baece143ee2180b394fc550bc3376ec2a0
NicolasRojas1 <nicrojasc6@gmail.com>
2023-04-02 18:03:37 -0500

    [add] creacion backend

---
+++ selocambio/src/main/java/com/selocambio/SelocambioApplication.java
@@ -0,0 +1,13 @@
+package com.selocambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelocambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelocambioApplication.class, args);
+	}
+
+}
