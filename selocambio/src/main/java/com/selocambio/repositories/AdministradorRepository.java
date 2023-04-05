905aee7d9d9fef751f030cd3c8ce9f2053e17064
Adolfo Sánchez Lopéz <sanchezlopezadolfo@gmail.com>
2023-04-04 21:44:07 -0500

    [ADD] Avance api para añadir admins usando postman

--- selocambio/src/main/java/com/selocambio/repositories/AdministradorRepository.java
+++ selocambio/src/main/java/com/selocambio/repositories/AdministradorRepository.java
@@ -1,8 +1,17 @@
 package com.selocambio.repositories;
 
 import com.selocambio.entities.AdministradorModel;
+import com.selocambio.entities.OfertaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface AdministradorRepository extends JpaRepository<AdministradorModel, Integer> {
+    @Query(value= "SELECT u from administrador u where u.nombre=:nombre" , nativeQuery = true )
+    List<AdministradorModel> buscarPorNombre(@Param("nombre") String nombre);
+
 }
--- selocambio/src/main/java/com/selocambio/repositories/OfertaRepository.java
+++ selocambio/src/main/java/com/selocambio/repositories/OfertaRepository.java
@@ -2,7 +2,21 @@ package com.selocambio.repositories;
 
 import com.selocambio.entities.OfertaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+
 @Repository
 public interface OfertaRepository extends JpaRepository<OfertaModel, Integer> {
+
+       @Query(value= "SELECT * from oferta where CategoriaProd=:categoria", nativeQuery = true)
+       List<OfertaModel> buscarPorCategoria(@Param("categoria") String categoria);
+
+       @Query(value= "SELECT * from oferta where NombreCli=:nombrecli", nativeQuery = true )
+       List<OfertaModel> buscarPorNombreCli(@Param("nombrecli") String nombreCli);
+
+
 }
