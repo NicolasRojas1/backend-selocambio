5f45439b6b380456ce9f1e1842f942d7c6f2867a
Jenny Cabrejo <jennycabrejomarin.com>
2023-04-05 21:50:05 -0500

    [ADD]- metodos oferta-e

--- selocambio/src/main/java/com/selocambio/services/OfertaService.java
+++ selocambio/src/main/java/com/selocambio/services/OfertaService.java
@@ -1,7 +1,31 @@
 package com.selocambio.services;
 
+import com.selocambio.entities.OfertaModel;
+import com.selocambio.repositories.OfertaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfertaService {
+
+    OfertaRepository ofertaRepository;
+
+    public OfertaService(OfertaRepository ofertaRepository) {
+        this.ofertaRepository = ofertaRepository;
+    }
+
+    public Object crearOferta(OfertaModel ofertaModel){
+        return ofertaRepository.save(ofertaModel);
+    }
+
+    public Object obtenerOferta(Integer idOferta){
+        return ofertaRepository.findById(idOferta);
+    }
+
+    public void eliminarOfertaById(Integer idOferta){
+        ofertaRepository.deleteById(idOferta);
+    }
+
+
+
+
 }
